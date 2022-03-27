d5d6bc5b7022509a5a81b473af648b79d1e69e66
karthickselvendran <selvendrankarthick@gmail.com>
2022-03-27 23:47:10 +0530

    bg clr changed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
@@ -14,20 +12,6 @@ function App() {
           <Route path="/sites" element={<h1>sites path</h1>} />
         </Routes>
       </BrowserRouter>
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
     </div>
   );
 }
