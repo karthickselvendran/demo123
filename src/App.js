7ed06b1aaf31b8897c2f1b84df7df4689450948c
karthickselvendran <selvendrankarthick@gmail.com>
2022-03-27 22:45:52 +0530

    route added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <h1>Routes....</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<h1>direct path</h1>} />
+          <Route path="/sites" element={<h1>sites path</h1>} />
+        </Routes>
+      </BrowserRouter>
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +27,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
     </div>
   );
 }
