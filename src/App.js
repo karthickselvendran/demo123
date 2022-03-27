10fb38200de8a24c60c74611f2a3bb6590a215cb
karthickselvendran <selvendrankarthick@gmail.com>
2022-03-28 00:00:57 +0530

    routes added

--- src/App.js
+++ src/App.js
@@ -8,8 +8,15 @@ function App() {
       <h1>Routes....</h1>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<h1>direct path</h1>} />
-          <Route path="/sites" element={<h1>sites path</h1>} />
+          <Route path='/' element={<h1>/ only</h1>} />
+          <Route path='signin' element={<h1>/ signin</h1>} />
+          <Route path='signup' element={<h1>/ signup</h1>} />
+          <Route path='todolist' element={<h1>/ todolist</h1>} />
+          <Route path='*' element={
+            <main className='wrongRoute'>
+              <p>There's nothing here, Please check the url!</p>
+            </main>
+          } />
         </Routes>
       </BrowserRouter>
     </div>
