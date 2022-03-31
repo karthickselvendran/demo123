93b075ca6d1b52a7811d93a8dcdf9cb9363e83ed
karthickselvendran <selvendrankarthick@gmail.com>
2022-04-01 01:07:44 +0530

    signup validations

--- src/pages/signup/signup.js
+++ src/pages/signup/signup.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { signupService } from '../../service/service';
 import { useNavigate, Link } from 'react-router-dom';
 import { toast } from 'react-toastify';
+import validator from 'validator';
 
 const initialState = {
     userName: '',
@@ -23,6 +24,15 @@ export const SignUp = () => {
 
     const handleChange = (e) => {
         const { name, value } = e.target;
+        if (name === "userName" && value.length > 10) {
+            return toast.error('Username should not exceeds 10 Digits!')
+        }
+        if (name === "phoneNumber" && value.length > 10) {
+            return toast.error('Phone Number must be 10 Digits!')
+        }
+        if (name === "password" && value.length > 20) {
+            return toast.error('Password must not exceeds 20 Digits!')
+        }
         setUserDetails(
             {
                 ...userDetails,
@@ -34,6 +44,15 @@ export const SignUp = () => {
         e.preventDefault();
         const { userName, email, phoneNumber, password } = userDetails
         if (!userName || !email || !phoneNumber || !password) return toast.error('All fields are required!')
+        if (userName < 4) return toast.error('Username must be minimum 4 Digits!')
+        if (!email.includes('@gmail.com')) return toast.error('Enter a valid Email!')
+        if (phoneNumber.length !== 10) return toast.error('Phone Number must be 10 Digits!')
+        if (!validator.isStrongPassword(password, {
+            minLength: 8, minLowercase: 1,
+            minUppercase: 1, minNumbers: 1, minSymbols: 1
+        })) {
+            return toast.error('Password must be strong, minLength: 8, minLowercase: 1, minUppercase: 1, minNumbers: 1, minSymbols: 1')
+        }
         signupService(userDetails)
             .then((res) => {
                 if (res.data.status === 200) {
